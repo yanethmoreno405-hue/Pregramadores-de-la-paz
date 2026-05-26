e76a07d71c28a8ba65342f7a4f9c1d927943cd62
danny yuleisa <yanethmoreno405@gmail.com>
2026-05-25 20:35:39 -0500

    actividad semana 4

---
+++ semana4/server.js
@@ -0,0 +1,13 @@
+pregunta:7
+R//
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando correctamente');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor ejecutándose en puerto 3000');
+});
+
